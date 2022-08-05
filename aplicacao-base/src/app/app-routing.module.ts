f23641c48ddceb80eb8ea426e0bc157266f57123
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2022-08-05 01:29:36 -0300

    estrutura inicial de um micro

---
+++ aplicacao-base/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule) },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
