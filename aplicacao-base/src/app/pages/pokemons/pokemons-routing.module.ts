5feae55216e900c7541cdffe6bced717388b4b88
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2022-08-27 19:14:56 -0300

    criando modulo de pokemons

--- aplicacao-base/src/app/pages/pokemons/pokemons-routing.module.ts
+++ aplicacao-base/src/app/pages/pokemons/pokemons-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UsersComponent } from './users.component';
+import { PokemonsComponent } from './pokemons.component';
 
-const routes: Routes = [{ path: '', component: UsersComponent }];
+const routes: Routes = [{ path: '', component: PokemonsComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class UsersRoutingModule { }
+export class PokemonsRoutingModule { }
--- aplicacao-base/src/app/pages/pokemons/pokemons.component.spec.ts
+++ aplicacao-base/src/app/pages/pokemons/pokemons.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UsersComponent } from './users.component';
+import { PokemonsComponent } from './pokemons.component';
 
-describe('UsersComponent', () => {
-  let component: UsersComponent;
-  let fixture: ComponentFixture<UsersComponent>;
+describe('PokemonsComponent', () => {
+  let component: PokemonsComponent;
+  let fixture: ComponentFixture<PokemonsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UsersComponent ]
+      declarations: [ PokemonsComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UsersComponent);
+    fixture = TestBed.createComponent(PokemonsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
